015b3df51c5579a34ed212c8168dca8958a82e23
szczawa <szczawinski0708@gmail.com>
2023-06-14 12:56:16 +0200

    check_llvm_ir

--- Test.py
+++ Test.py
@@ -1,13 +1,17 @@
 from antlr4 import *
+import antlr4
 from MY_LANGLexer import MY_LANGLexer
 from MY_LANGParser import MY_LANGParser
 from antlr4.tree.Trees import Trees
 from MyListener import MyListener
 
+from llvmlite import ir
+from llvmlite import binding
+
 def main():
 
     # Open input file
-    with open("Testy/Func.txt", "r") as file:
+    with open("Testy/PlikTestowy.txt", "r") as file:
         input_code = file.read()
     input_stream = InputStream(input_code)
 
@@ -28,6 +32,12 @@ def main():
     # Start parsing from the 'prog' rule 
     tree = parser.prog()
 
+
+    for node in tree.children:
+        if type(node) is antlr4.tree.Tree.ErrorNodeImpl:
+            print(">>>Blad generacji kodu")
+            return
+
     # printing parse tree
     print(Trees.toStringTree(tree, None, parser))
 
@@ -35,7 +45,28 @@ def main():
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
     whole_program_LLVM = printer.gen.print_main_text()
-    print(whole_program_LLVM)
-    # print(printer.gen.main_text)
+    #print(whole_program_LLVM)
+
+    def check_llvm_ir(llvm_ir):
+    # Tworzenie kontekstu i modułu LLVM
+        context = ir.Context()
+        module = ir.Module(context)
+        
+        try:
+            # Wczytywanie kodu LLVM IR 
+            module = binding.parse_assembly(llvm_ir)
+            
+            # Sprawdzanie poprawności
+            module.verify()
+            print("Kod LLVM IR jest poprawny.")
+            
+        except Exception as e:
+            print("Błąd w kodzie LLVM IR:")
+            print(str(e))
+    
+    check_llvm_ir(whole_program_LLVM)
+
+   
+
 if __name__ == '__main__':
     main()
